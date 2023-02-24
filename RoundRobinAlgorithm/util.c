512023a06387871eafb5c1c3a88d6e10e789e8b4
Yeo Wen Qin, Kaynn <KaynnCahya@outlook.com>
2023-02-24 18:16:01 +0800

    Created function to generate random processes

--- RoundRobinAlgorithm/util.c
+++ RoundRobinAlgorithm/util.c
@@ -2,9 +2,33 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
+#include <time.h>
 
+#include "Models/process.h"
 #include "Models/process_result.h"
 #include "Models/round_robin_result.h"
+
+#define MAX_BURST_TIME 54
+#define MIN_BURST_TIME 1
+
+#define MAX_ARRIVAL_TIME 20
+
+Process* generate_random_processes(processesSize){
+	Process* processes = malloc(processesSize * sizeof(Process));
+	srand(time(NULL));
+	int i;
+	for (i = 0; i < processesSize; ++i){
+		char[MAX_NAME_LEN] processName;
+		snprintf(processName, MAX_NAME_LEN, "P%d", i);
+		strcpy(processes[i].name, processName);
+
+		float generatedBurstTime = ((float) rand() / RAND_MAX) * (MAX_BURST_TIME - MIN_BURST_TIME) + MIN_BURST_TIME;
+		float generatedArrivalTime = ((float) rand() / RAND_MAX) * (MAX_ARRIVAL_TIME);
+		processes[i].burstTime = generatedBurstTime;
+		processes[i].arrivalTime = generatedArrivalTime;
+	}
+	return processes;
+}
 
 void append_main_result(FILE* filePointer, RoundRobinResult result){
 	if (filePointer == NULL){return;}
