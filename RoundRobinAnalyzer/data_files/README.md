783fea921c789d1fe51df9f91d0d3e9b0fa107d1
Yeo Wen Qin, Kaynn <KaynnCahya@outlook.com>
2023-02-27 23:14:00 +0800

    Read multiple data files and group by grouping

---
+++ RoundRobinAnalyzer/data_files/README.md
@@ -0,0 +1,1 @@
+Round Robin algorithm data files `.dat` should go here for analysis.
---
+++ RoundRobinAnalyzer/models/analysis_group.py
@@ -0,0 +1,6 @@
+from models.analysis_data import AnalysisData
+
+class AnalysisGroup:
+    def __init__(self) -> None:
+        self.data_group: list[AnalysisData] = []
+        pass
